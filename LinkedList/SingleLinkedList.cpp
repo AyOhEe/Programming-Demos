bd60ab7a98c09509dbd02f7f673c094270fec8ab
Aria <awesomepantsap@gmail.com>
2024-09-13 12:09:46 +0100

    Function signatures for SingleLinkedList

--- LinkedList/SingleLinkedList.cpp
+++ LinkedList/SingleLinkedList.cpp
@@ -1,7 +1,50 @@
 #include "SingleLinkedList.hpp"
 
 template <typename T>
-SingleLinkedListNode<T>::SingleLinkedListNode(T* _value, SingleLinkedListNode* _next) {
+SingleLinkedListNode<T>::SingleLinkedListNode(T* _value) {
 	next = _next;
 	value = _value;
+}
+
+
+template <typename T>
+SingleLinkedListNode<T>* SingleLinkedList<T>::seek(int i) {
+
+}
+
+template <typename T>
+SingleLinkedList<T>::SingleLinkedList<T>() {
+	first = nullptr;
+	last = nullptr;
+	length = 0;
+}
+
+template <typename T>
+T* SingleLinkedList<T>::get(int i) {
+
+}
+
+template <typename T>
+void SingleLinkedList<T>::set(int i, T* value) {
+
+}
+
+template <typename T>
+bool SingleLinkedList<T>::remove(int i) {
+
+}
+
+template <typename T>
+void SingleLinkedList<T>::insert(T* value, int atIndex) {
+
+}
+
+template <typename T>
+void SingleLinkedList<T>::insertFront(T* value) {
+
+}
+
+template <typename T>
+void SingleLinkedList<T>::append(T* value) {
+
 }
--- LinkedList/SingleLinkedList.hpp
+++ LinkedList/SingleLinkedList.hpp
@@ -1,9 +1,29 @@
-#pragma once
-
 template <typename T>
 struct SingleLinkedListNode {
 	SingleLinkedListNode<T>* next;
 	T* value;
 
-	SingleLinkedListNode<T>(T* _value, SingleLinkedListNode* _next);
+	SingleLinkedListNode<T>(T* _value);
+};
+
+template <typename T>
+struct SingleLinkedList {
+private:
+	SingleLinkedListNode<T>* first;
+	SingleLinkedListNode<T>* last;
+	int length;
+
+	SingleLinkedListNode<T>* seek(int i);
+
+public:
+	SingleLinkedList<T>();
+
+	T* get(int i);
+	void set(int i, T* value);
+
+	bool remove(int i);
+
+	void insert(T* value, int atIndex);
+	void insertFront(T* value);
+	void append(T* value);
 };
